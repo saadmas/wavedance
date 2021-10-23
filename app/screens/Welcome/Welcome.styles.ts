cc6725354832671c87a0e7d67a090c27117e23ea
Saad <saadmasood96@hotmail.com>
2021-10-22 22:15:24 -0400

    create super basic app directory outline, install react-navigation package

---
+++ app/screens/Welcome/Welcome.styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const WelcomeStyles = StyleSheet.create({
+  container: {
+    backgroundColor: '#222222',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  authButtons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+});
---
+++ app/screens/Welcome/Welcome.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+import { WelcomeStyles } from './Welcome.styles';
+
+const Welcome = () => {
+  return <View style={WelcomeStyles.container}>WELCOME!</View>;
+};
+
+export default Welcome;
