02243e6a38d166f37cda45e4109ed0ef21e1f9a2
Saad <saadmasood96@hotmail.com>
2021-10-29 22:42:43 -0400

    refactor to use less nested screens for auth

--- app/stacks/AuthStack/screens/Welcome/Welcome.tsx
+++ app/stacks/AuthStack/screens/Welcome/Welcome.tsx
@@ -2,24 +2,20 @@ import { styles } from './Welcome.styles';
 import * as React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { Button } from 'react-native-paper';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { AuthStackParamList } from '../../AuthStack';
-import { Path } from '../../../../routing/paths';
 
-interface WelcomeProps extends NativeStackScreenProps<AuthStackParamList, Path.Welcome> {}
+interface WelcomeProps {
+  onSignUp: () => void;
+  onLogin: () => void;
+}
 
-const Welcome = ({ navigation }: WelcomeProps) => {
-  const onSignUpPress = () => {
-    navigation.navigate(Path.SignUp);
-  };
-
+const Welcome = ({ onSignUp, onLogin }: WelcomeProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.authButtonGroup}>
-        <Button mode="contained" uppercase={false} style={styles.signUpButton} onPress={onSignUpPress}>
+        <Button mode="contained" uppercase={false} style={styles.signUpButton} onPress={onSignUp}>
           Sign up
         </Button>
-        <TouchableOpacity underlayColor="#fff">
+        <TouchableOpacity onPress={onLogin}>
           <Text style={styles.login}>Login</Text>
         </TouchableOpacity>
       </View>
