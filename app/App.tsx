30f26eeb595c148607fb04cf6d7dff85b4d85d05
Saad <saadmasood96@hotmail.com>
2021-11-10 14:30:42 -0500

    try to get token from store on app launch

--- app/App.tsx
+++ app/App.tsx
@@ -2,22 +2,41 @@ import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import theme from './styles/theme';
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { View } from 'react-native';
 import { styles } from './App.styles';
 import AuthScreen from './screens/AuthScreen/AuthScreen';
 import { navigationRef } from './routing/rootNavigation';
 import { Montserrat_400Regular, Lustria_400Regular, useFonts } from '@expo-google-fonts/dev'; //* delete and change to use reg package when done
 import AppLoading from 'expo-app-loading';
+import * as SecureStore from 'expo-secure-store';
+import { useAuthState, useAuthUpdater } from './state/auth/AuthProvider';
 
 const App = () => {
-  const isSignedIn = false;
+  const userToken = useAuthState();
+  const setUserToken = useAuthUpdater();
+
+  const [haveAttempedToGetUserToken, setHaveAttempedToGetUserToken] = React.useState<boolean>(false);
 
   const [fontsLoaded] = useFonts({
     Montserrat_400Regular,
     Lustria_400Regular,
   });
 
-  if (!fontsLoaded) {
+  React.useEffect(() => {
+    const tryGetUserToken = async () => {
+      try {
+        const secureStoreToken = await SecureStore.getItemAsync('userToken');
+        if (secureStoreToken) {
+          setUserToken(secureStoreToken);
+        }
+      } catch {}
+      setHaveAttempedToGetUserToken(true);
+    };
+
+    tryGetUserToken();
+  }, []);
+
+  if (!fontsLoaded || !haveAttempedToGetUserToken) {
     return <AppLoading />;
   }
 
@@ -25,7 +44,7 @@ const App = () => {
     <PaperProvider theme={theme}>
       <View style={styles.app}>
         <NavigationContainer ref={navigationRef} theme={theme}>
-          {isSignedIn ? null : <AuthScreen />}
+          {userToken ? null : <AuthScreen />}
         </NavigationContainer>
       </View>
     </PaperProvider>
