aee6edec76352b10e6d59a1218a4fa75eeca35be
Saad <saadmasood96@hotmail.com>
2021-12-14 13:46:43 -0500

    handle loading and error states for user profile image

--- app/components/SpotifyEmbed/SpotifyEmbed.tsx
+++ app/components/SpotifyEmbed/SpotifyEmbed.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@react-navigation/native';
 import * as React from 'react';
 import { ScrollView } from 'react-native';
 import { WebView } from 'react-native-webview';
@@ -7,14 +8,25 @@ interface SpotifyEmbedProps {
 }
 
 const SpotifyEmbed = ({ uri }: SpotifyEmbedProps) => {
+  const { colors } = useTheme();
+  const [isError, setIsError] = React.useState<boolean>(!uri);
   const marginVertical = 10;
+
+  React.useEffect(() => {
+    setIsError(!uri);
+  }, [uri]);
+
+  const onEmbedError = () => {
+    setIsError(true);
+  };
 
   return uri ? (
     <ScrollView>
       <WebView
         bounces={false}
+        onError={onEmbedError}
         javaScriptEnabled={true}
-        style={{ height: 80, width: '100%', marginVertical }}
+        style={{ height: 80, width: '100%', marginVertical, backgroundColor: colors.background }}
         source={{ uri }}
       />
     </ScrollView>
--- app/components/UserProfileQuery/UserProfile/UserProfileImage/UserProfileImage.tsx
+++ app/components/UserProfileQuery/UserProfile/UserProfileImage/UserProfileImage.tsx
@@ -1,27 +1,44 @@
 import * as React from 'react';
-import { Image } from 'react-native';
+import { Image, ImageBackground, View } from 'react-native';
+import { ActivityIndicator, useTheme } from 'react-native-paper';
+import { ResponseStatus } from '../../../../state/enums/responseStatus';
 
 interface UserProfileImageProps {
   userPhotoUri: string;
 }
 
 const UserProfileImage = ({ userPhotoUri }: UserProfileImageProps) => {
+  const [responseStatus, setResponseStatus] = React.useState<ResponseStatus>(ResponseStatus.Loading);
+  const height = 350;
+  const width = '100%';
+
   const onError = () => {
     //* use WD image
+    setResponseStatus(ResponseStatus.Error);
   };
 
-  if (!userPhotoUri) {
-    return null;
-  }
+  const onLoad = () => {
+    setResponseStatus(ResponseStatus.Success);
+  };
+
+  const renderLoader = () => {
+    if (responseStatus === ResponseStatus.Loading) {
+      return <ActivityIndicator size={40} style={{ alignItems: 'center', height: '90%' }} />;
+    }
+  };
 
   return (
-    <Image
-      onError={onError}
-      source={{ uri: userPhotoUri }}
-      style={{ height: 350, width: '100%' }}
-      borderRadius={10}
-      resizeMode="cover"
-    />
+    <>
+      <ImageBackground
+        onError={onError}
+        onLoad={onLoad}
+        source={{ uri: userPhotoUri || undefined }}
+        style={{ height, width, backgroundColor: '#333', borderRadius: 10 }}
+        resizeMode="cover"
+      >
+        {renderLoader()}
+      </ImageBackground>
+    </>
   );
 };
 
