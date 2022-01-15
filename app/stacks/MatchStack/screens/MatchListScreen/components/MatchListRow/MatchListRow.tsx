a7663eee1524fa2af3d1dc211e6a086d57db74fc
Saad <saadmasood96@hotmail.com>
2022-01-15 23:11:40 +0500

    clip text and add You: to last message sent if sent by curr user

--- app/stacks/MatchStack/screens/MatchListScreen/components/MatchListRow/MatchListRow.tsx
+++ app/stacks/MatchStack/screens/MatchListScreen/components/MatchListRow/MatchListRow.tsx
@@ -13,12 +13,27 @@ const MatchListRow = ({ name, photoUri, lastMessageSent }: MatchListRowProps) =>
   const { fonts, colors } = useTheme();
   const color = lastMessageSent?.isRead ? colors.text : colors.primary;
 
+  const getClippedText = (text: string, maxSize: number) => {
+    if (text.length < maxSize) {
+      return text;
+    }
+    return `${text.substring(0, maxSize)}...`;
+  };
+
+  const getLastMessageText = () => {
+    //f const uid = firebase.auth().currentUser?.uid ?? 'foo';
+    const uid = 'foo';
+    const messageText = lastMessageSent?.sentBy === uid ? `You: ${lastMessageSent?.message}` : lastMessageSent?.message;
+    const clippedMessageText = getClippedText(messageText ?? '', 40);
+    return clippedMessageText;
+  };
+
   return (
     <View style={{ flexDirection: 'row', paddingVertical: 10, alignItems: 'center' }}>
       <Avatar.Image source={{ uri: photoUri }} size={60} />
       <View style={{ marginLeft: 10 }}>
-        <Text style={{ fontFamily: fonts.light.fontFamily, color }}>{name}</Text>
-        <Text style={{ fontSize: 12, color }}>{lastMessageSent?.message}</Text>
+        <Text style={{ fontFamily: fonts.light.fontFamily, color }}>{getClippedText(name, 30)}</Text>
+        <Text style={{ fontSize: 12, color }}>{getLastMessageText()}</Text>
       </View>
     </View>
   );
